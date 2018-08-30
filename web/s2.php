9fe602b1671b46e44b193916356cc6e2a368c288
RXMASTER <42844151+RXMASTER@users.noreply.github.com>
2018-08-30 11:35:11 -0500

    Update s2.php

--- web/s2.php
+++ web/s2.php
@@ -1,18 +1,1 @@
 
-<?php
-$id = $_GET['id'];
-$ticket = $_GET['ticket'];
-$captcha_response = $_GET['captcha_response'];
-$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL, "https://api.openload.co/1/file/dl?file=".$id."&ticket=".$ticket."&captcha_response=".$captcha_response."");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_HEADER, FALSE);
-
-curl_setopt($ch, CURLOPT_POSTFIELDS, "https://api.openload.co/1/file/dl?file=".$id."&ticket=".$ticket."&captcha_response=".$captcha_response."");
-
-$response = curl_exec($ch);
-curl_close($ch);
-
-//var_dump($response);
-echo $response;
